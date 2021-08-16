66bf9e3fcf48ffdf9af2f8a05b7175f4092a1468
Yair Nahum <yair.y.nahum@intel.com>
2021-08-17 01:13:08 +0300

    use func positional args

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -44,44 +44,36 @@ def calc_f1_value_grad_hessian(x, A, calc_grad=True, calc_hessian=True):
     return ret
 
 
-def h_val_der_sec_der(x, calc_g=True, calc_h=True):
-    h_val = np.sqrt(1 + (x ** 2))
-    to_ret = [h_val]
-    if calc_g:
-        h_der = x/np.sqrt(1+(x**2))
-        to_ret.append(h_der)
-    if calc_h:
-        h_sec_der = 1 / np.power(1 + (x ** 2), 1.5)
-        to_ret.append(h_sec_der)
-    return to_ret
+def calc_h_value_and_derivatives(x, calc_first_derivative=True, calc_second_derivative=True):
+    h_value = np.sqrt(1 + (x ** 2))
+    ret = [h_value]
+    if calc_first_derivative:
+        ret.append(x/np.sqrt(1+(x**2)))
+    if calc_second_derivative:
+        ret.append(1 / np.power(1 + (x ** 2), 1.5))
+    return ret
 
 
-def f2_val_grad_hessian(x, calc_grad=True, calc_hessian=True):
-    phi_val, phi_grad, phi_hessian = calc_phi_value_grad_hessian(x)
-    h_val, h_der, h_sec_der = h_val_der_sec_der(phi_val)
-    f2_val = h_val
-    to_ret = [f2_val]
+def calc_f2_value_grad_hessian(x, calc_grad=True, calc_hessian=True):
+    phi_value, phi_grad, phi_hessian = calc_phi_value_grad_hessian(x)
+    h_value, h_first_derivative, h_second_derivative = calc_h_value_and_derivatives(phi_value)
+    ret = [h_value]
     if calc_grad:
-        f2_grad = h_der * phi_grad
-        to_ret.append(f2_grad)
+        ret.append(h_first_derivative * phi_grad)
     if calc_hessian:
-        f2_hessian = h_der * phi_hessian + h_sec_der * np.matmul(phi_grad, np.transpose(phi_grad))
-        to_ret.append(f2_hessian)
-    return to_ret
+        ret.append(h_first_derivative * phi_hessian + h_second_derivative * (phi_grad @ phi_grad.T))
+    return ret
 
-# 1.2
-def numerical_evaluation(f, x, eps, A=None):
+
+# 1.2.3
+def evaluate_grad_hessian(x, eps, func, func_pos_args):
     n = x.shape[0]
     I = np.identity(n) * eps
     grad = np.zeros((n, 1))
     hessian = np.zeros((n, n))
     for i in range(n):
-        if A is None:
-            f_val_add, f_g_add = f(x + np.expand_dims(I[:, i], 1), calc_grad=True, calc_hessian=False)
-            f_val_sub, f_g_sub = f(x - np.expand_dims(I[:, i], 1), calc_grad=True, calc_hessian=False)
-        else:
-            f_val_add, f_g_add = f(x + np.expand_dims(I[:, i], 1), A, calc_grad=True, calc_hessian=False)
-            f_val_sub, f_g_sub = f(x - np.expand_dims(I[:, i], 1), A, calc_grad=True, calc_hessian=False)
+        f_val_add, f_g_add = func(x + np.expand_dims(I[:, i], 1), *func_pos_args)
+        f_val_sub, f_g_sub = func(x - np.expand_dims(I[:, i], 1), *func_pos_args)
 
         grad[i] = (f_val_add - f_val_sub) / (2 * eps)
         hessian[:, i] = ((f_g_add - f_g_sub) / (2 * eps)).squeeze(1)
@@ -96,7 +88,7 @@ def comparison():
     x = np.random.rand(3, 1)
     A = np.random.rand(3, 3)
     _, f1_g, f1_h = calc_f1_value_grad_hessian(x, A)
-    _, f2_g, f2_h = f2_val_grad_hessian(x)
+    _, f2_g, f2_h = calc_f2_value_grad_hessian(x)
 
     f1_g_norm = []
     f1_h_norm = []
@@ -107,16 +99,25 @@ def comparison():
     x_axis = np.arange(61)
 
     for i in x_axis:
-        f1_num_g, f1_num_h = numerical_evaluation(calc_f1_value_grad_hessian, x,
-                                                  np.power(2.0, -i), A)
+        f1_num_g, f1_num_h =\
+            evaluate_grad_hessian(
+                x,
+                np.power(2.0, -i),
+                calc_f1_value_grad_hessian,
+                [A, True, False])
 
         f1_g_diff = np.linalg.norm(f1_g-f1_num_g, ord=np.inf)
         f1_g_norm.append(f1_g_diff)
         f1_h_diff = np.linalg.norm(f1_h-f1_num_h, ord=np.inf)
         f1_h_norm.append(f1_h_diff)
 
-        f2_num_g, f2_num_h = numerical_evaluation(f2_val_grad_hessian, x,
-                                                  np.power(2.0, -i))
+        f2_num_g, f2_num_h =\
+            evaluate_grad_hessian(
+                x,
+                np.power(2.0, -i),
+                calc_f2_value_grad_hessian,
+                [True, False])
+
         f2_g_diff = np.linalg.norm(f2_g-f2_num_g, ord=np.inf)
         f2_g_norm.append(f2_g_diff)
         f2_h_diff = np.linalg.norm(f2_h-f2_num_h, ord=np.inf)
