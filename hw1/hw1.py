61dcd06259b510a4e01d9981e4ee83f0b1ee21a2
Yair Nahum <yair.y.nahum@intel.com>
2021-08-18 18:38:47 +0300

    trying to remove the squeeze thing, still not working ok with hessian of f_2

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,77 +4,82 @@ from f1 import calc_f1_value_grad_hessian
 from f2 import calc_f2_value_grad_hessian
 from func_cmp_analyt_vs_num import func_compare_analytical_vs_numerical
 
+run_on_f1 = False
+run_on_f2 = True
 
 def compare_analytical_vs_numerical():
 
-    np.random.seed(10)
+    np.random.seed(42)
 
     exp_range = np.arange(61)
     eps_array = np.power(2.0, -exp_range)
 
-    x = np.random.rand(3, 1)
-    A = np.random.rand(3, 3)
-    f1_grad_diff_max_norm, f1_hessian_diff_max_norm = func_compare_analytical_vs_numerical(x, eps_array, calc_f1_value_grad_hessian, [A])
+    x = np.random.rand(3,)
 
-    plt.subplot()
-    plt.plot(exp_range, f1_grad_diff_max_norm)
-    plt.suptitle(r'$\log{||\nabla_{x}^{analytical}f_1 - \nabla_{x}^{numerical}f_1||_{\infty}}$ vs  $\epsilon$ accuracy')
-    plt.xlabel(r'Absolute value of $\epsilon$ exponent')
-    plt.yscale('log')
-    plt.show()
+    if run_on_f1:
+        A = np.random.rand(3, 3)
+        f1_grad_diff_max_norm, f1_hessian_diff_max_norm = func_compare_analytical_vs_numerical(x, eps_array, calc_f1_value_grad_hessian, [A])
 
-    plt.plot(exp_range, f1_hessian_diff_max_norm)
-    plt.suptitle(r'$\log{||\nabla_{x}^{2\;analytical}f_1 - \nabla_{x}^{2\;numerical}f_1||_{\infty}}$ vs  $\epsilon$ accuracy')
-    plt.xlabel(r'Absolute value of $\epsilon$ exponent')
-    plt.yscale('log')
-    plt.show()
+        plt.subplot()
+        plt.plot(exp_range, f1_grad_diff_max_norm)
+        plt.suptitle(r'$\log{||\nabla_{x}^{analytical}f_1 - \nabla_{x}^{numerical}f_1||_{\infty}}$ vs  $\epsilon$ accuracy')
+        plt.xlabel(r'Absolute value of $\epsilon$ exponent')
+        plt.yscale('log')
+        plt.show()
 
-    f1_grad_diff_max_norm_min = np.min(f1_grad_diff_max_norm)
-    f1_grad_diff_max_norm_argmin = np.argmin(f1_grad_diff_max_norm)
+        plt.plot(exp_range, f1_hessian_diff_max_norm)
+        plt.suptitle(r'$\log{||\nabla_{x}^{2\;analytical}f_1 - \nabla_{x}^{2\;numerical}f_1||_{\infty}}$ vs  $\epsilon$ accuracy')
+        plt.xlabel(r'Absolute value of $\epsilon$ exponent')
+        plt.yscale('log')
+        plt.show()
 
-    print(f'f1 gradient min infinity norm error : {f1_grad_diff_max_norm_min} ')
-    print(f'epsilon exponent: {-f1_grad_diff_max_norm_argmin}')
-    print(f'epsilon value: {np.power(2.0, -f1_grad_diff_max_norm_argmin)}')
-    print()
+        f1_grad_diff_max_norm_min = np.min(f1_grad_diff_max_norm)
+        f1_grad_diff_max_norm_argmin = np.argmin(f1_grad_diff_max_norm)
 
-    f1_hessian_diff_max_norm_min = np.min(f1_hessian_diff_max_norm)
-    f1_hessian_diff_max_norm_argmin = np.argmin(f1_hessian_diff_max_norm)
+        print(f'f1 gradient min infinity norm error : {f1_grad_diff_max_norm_min} ')
+        print(f'epsilon exponent: {-f1_grad_diff_max_norm_argmin}')
+        print(f'epsilon value: {np.power(2.0, -f1_grad_diff_max_norm_argmin)}')
+        print()
 
-    print(f'f1 hessian min infinity norm error : {f1_hessian_diff_max_norm_min} ')
-    print(f'epsilon exponent: {-f1_hessian_diff_max_norm_argmin}')
-    print(f'epsilon value: {np.power(2.0, -f1_hessian_diff_max_norm_argmin)}')
-    print()
+        f1_hessian_diff_max_norm_min = np.min(f1_hessian_diff_max_norm)
+        f1_hessian_diff_max_norm_argmin = np.argmin(f1_hessian_diff_max_norm)
 
-    f2_grad_diff_max_norm, f2_hessian_diff_max_norm = func_compare_analytical_vs_numerical(x, eps_array, calc_f2_value_grad_hessian, [])
+        print(f'f1 hessian min infinity norm error : {f1_hessian_diff_max_norm_min} ')
+        print(f'epsilon exponent: {-f1_hessian_diff_max_norm_argmin}')
+        print(f'epsilon value: {np.power(2.0, -f1_hessian_diff_max_norm_argmin)}')
+        print()
 
-    plt.subplot()
-    plt.plot(exp_range, f2_grad_diff_max_norm)
-    plt.suptitle(r'$\log{||\nabla_{x}^{analytical}f_2 - \nabla_{x}^{numerical}f_2||_{\infty}}$ vs  $\epsilon$ accuracy')
-    plt.xlabel(r'Absolute value of $\epsilon$ exponent')
-    plt.yscale('log')
-    plt.show()
+    if run_on_f2:
+        f2_grad_diff_max_norm, f2_hessian_diff_max_norm = func_compare_analytical_vs_numerical(x, eps_array, calc_f2_value_grad_hessian, [])
 
-    plt.plot(exp_range, f2_hessian_diff_max_norm)
-    plt.suptitle(r'$\log{||\nabla_{x}^{2\;analytical}f_2 - \nabla_{x}^{2\;numerical}f_2||_{\infty}}$ vs  $\epsilon$ accuracy')
-    plt.xlabel(r'Absolute value of $\epsilon$ exponent')
-    plt.yscale('log')
-    plt.show()
+        plt.subplot()
+        plt.plot(exp_range, f2_grad_diff_max_norm)
+        plt.suptitle(r'$\log{||\nabla_{x}^{analytical}f_2 - \nabla_{x}^{numerical}f_2||_{\infty}}$ vs  $\epsilon$ accuracy')
+        plt.xlabel(r'Absolute value of $\epsilon$ exponent')
+        plt.yscale('log')
+        plt.show()
 
-    f2_grad_diff_max_norm_min = np.min(f2_grad_diff_max_norm)
-    f2_grad_diff_max_norm_argmin = np.argmin(f2_grad_diff_max_norm)
+        plt.plot(exp_range, f2_hessian_diff_max_norm)
+        plt.suptitle(r'$\log{||\nabla_{x}^{2\;analytical}f_2 - \nabla_{x}^{2\;numerical}f_2||_{\infty}}$ vs  $\epsilon$ accuracy')
+        plt.xlabel(r'Absolute value of $\epsilon$ exponent')
+        plt.yscale('log')
+        plt.show()
 
-    print(f'f2 gradient min infinity norm error : {f2_grad_diff_max_norm_min} ')
-    print(f'epsilon exponent: {-f2_grad_diff_max_norm_argmin}')
-    print(f'epsilon value: {np.power(2.0, -f2_grad_diff_max_norm_argmin)}')
-    print()
+        f2_grad_diff_max_norm_min = np.min(f2_grad_diff_max_norm)
+        f2_grad_diff_max_norm_argmin = np.argmin(f2_grad_diff_max_norm)
 
-    f2_hessian_diff_max_norm_min = np.min(f2_hessian_diff_max_norm)
-    f2_hessian_diff_max_norm_argmin = np.argmin(f2_hessian_diff_max_norm)
+        print(f'f2 gradient min infinity norm error : {f2_grad_diff_max_norm_min} ')
+        print(f'epsilon exponent: {-f2_grad_diff_max_norm_argmin}')
+        print(f'epsilon value: {np.power(2.0, -f2_grad_diff_max_norm_argmin)}')
+        print()
 
-    print(f'f2 hessian min infinity norm error : {f2_hessian_diff_max_norm_min} ')
-    print(f'epsilon exponent: {-f2_hessian_diff_max_norm_argmin}')
-    print(f'epsilon value: {np.power(2.0, -f2_hessian_diff_max_norm_argmin)}')
-    print()
+        f2_hessian_diff_max_norm_min = np.min(f2_hessian_diff_max_norm)
+        f2_hessian_diff_max_norm_argmin = np.argmin(f2_hessian_diff_max_norm)
+
+        print(f'f2 hessian min infinity norm error : {f2_hessian_diff_max_norm_min} ')
+        print(f'epsilon exponent: {-f2_hessian_diff_max_norm_argmin}')
+        print(f'epsilon value: {np.power(2.0, -f2_hessian_diff_max_norm_argmin)}')
+        print()
 
 
 if __name__ == '__main__':
--- hw1/phi.py
+++ hw1/phi.py
@@ -2,7 +2,6 @@ import numpy as np
 
 # 1.1.5
 def calc_phi_value_grad_hessian(x, calc_grad=True, calc_hessian=True):
-    x_vec = x.squeeze()
     u_value = np.prod(x, axis=0)
     sin_value = np.sin(u_value)
     cos_value = np.cos(u_value)
@@ -16,15 +15,15 @@ def calc_phi_value_grad_hessian(x, calc_grad=True, calc_hessian=True):
         phi_grad = u_grad * cos_value
         ret.append(phi_grad)
     if calc_hessian:
-        H1 = np.array([[0, x_vec[2], x_vec[1]],
-                       [x_vec[2], 0, x_vec[0]],
-                       [x_vec[1], x_vec[0], 0]])
-        H2_11 = (x_vec[1]*x_vec[2])**2
-        H2_12 = x_vec[0] * x_vec[1] * (x_vec[2]**2)
-        H2_13 = x_vec[0] * (x_vec[1]**2) * x_vec[2]
-        H2_22 = (x_vec[0]*x_vec[2])**2
-        H2_23 = (x_vec[0]**2) * x_vec[1] * x_vec[2]
-        H2_33 = (x_vec[0] * x_vec[1])**2
+        H1 = np.array([[0, x[2], x[1]],
+                       [x[2], 0, x[0]],
+                       [x[1], x[0], 0]])
+        H2_11 = (x[1]*x[2])**2
+        H2_12 = x[0] * x[1] * (x[2]**2)
+        H2_13 = x[0] * (x[1]**2) * x[2]
+        H2_22 = (x[0]*x[2])**2
+        H2_23 = (x[0]**2) * x[1] * x[2]
+        H2_33 = (x[0] * x[1])**2
         H2 = np.array([[H2_11, H2_12, H2_13],
                        [H2_12, H2_22, H2_23],
                        [H2_13, H2_23, H2_33]])
