5279fc9bed7a2c61de9e7972cada7ebca8d4ce3d
Yair Nahum <yair.y.nahum@intel.com>
2021-08-23 16:59:03 +0300

    hw2 rosenbrock start

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -2,6 +2,7 @@ import numpy as np
 from grad_descent import grad_descent
 from newton_method import newton_method
 from plot_utils import plot_search_method_graphs
+from rosenbrock import *
 
 if __name__ == '__main__':
 
@@ -30,7 +31,7 @@ if __name__ == '__main__':
 
     exact_line_search_config = [ True, True, True, False, False, False]
 
-    run_grad_descent = True
+    run_grad_descent = False
 
     if run_grad_descent:
         for idx, Q in enumerate(Qs):
@@ -38,10 +39,12 @@ if __name__ == '__main__':
                 x_0s[idx], Q, exact_line_search=exact_line_search_config[idx])
             plot_search_method_graphs(Q, trajectory_points)
 
-    run_newton_method = True
+    run_newton_method = False
 
     if run_newton_method:
         for idx, Q in enumerate(Qs):
             trajectory_points = newton_method(
                 x_0s[idx], Q, exact_line_search=exact_line_search_config[idx])
             plot_search_method_graphs(Q, trajectory_points)
+
+    #run_rosenbrock_test = False
---
+++ hw2/rosenbrock.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# 2.7
+def rosenbrock_func(x):
+    f = 0
+    for i in range(len(x)-1):
+        f += ((100 * x[i+1] - x[i] ** 2) ** 2 + (1 - x[i] ** 2))
+    return f
+
+
+def rosenbrock_grad(x):
+    n = len(x)
+    assert(len >=2)
+    grad = np.zeros_like(x)
+    grad[0] = 400 * x[0] * (x[0] ** 2 - x[1]) + 2 * x[0] - 2
+    for i in range(1,(n-1)):
+        grad[i] = 400 * x[i] * (x[i] ** 2 - x[i+1]) + 202 * x[i] - 200 * (x[i-1] ** 2) - 2
+    grad[n-1] = 200 * (x[n-1] - x[n - 2] ** 2)
+    return grad
+
+
+def rosenbrock_hessian(x):
+    n = len(x)
+    assert(len >=2)
+    hessian = np.zeros((n,n))
+
+    hessian[0][0] = 1200 * (x[0] ** 2) - 400 * x[1] + 2
+    hessian[0][1] = -400 * x[0]
+    for i in range(1,(n-1)):
+        hessian[i][i-1] = -400 * x[i-1]
+        hessian[i][i] = 1200 * (x[i] ** 2) - 400 * x[i+1] + 202
+        hessian[i][i+1] =  -400 * x[i]
+    hessian[n-1][n-2] = -400 * x[n-2]
+    hessian[n-1][n-1] = 200
+    return hessian
