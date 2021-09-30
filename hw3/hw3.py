26428d4c0115f3151bb05e2c0bf6fc330bf6c579
Yair Nahum <yair.y.nahum@intel.com>
2021-09-30 12:51:44 +0300

    hw3 move loss and phi into model class

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -5,52 +5,29 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 
-# 1.3.5
+
 def f(x_1, x_2):
     return x_1 * np.exp(-(x_1 ** 2 + x_2 ** 2))
 
 
-# 1.3.6
-def phi(x):
-    return np.tanh(x)
-
-
-def g_phi(x):
-    return 1 - (np.tanh(x) ** 2)
-
-
-def loss(F, y):
-    return (F - y)**2
-
-
-# 1.3.7
-def g_loss(F, y):
-    return 2 * (F - y)
-
-
-# 1.3.10-11
-def get_data(num_of_samples, sample_vec_size):
-    X = 4 * np.random.rand(num_of_samples, sample_vec_size) - 2
-    labels = []
-    for x in X:
-        labels.append(f(x))
-    return X, np.array(labels)
-
-
-# 1.3.8,9,12
+# 1.3.5-9,12
 class My_FC_NN_Model(object):
-    def __init__(self, in_size, loss_type='mse'):
+    def __init__(self, in_size, loss_type='mse', activation_type='tanh'):
         self.layers = {}
         self.layer_index = 0
         self.loss_type = loss_type
+        self.activation_type = activation_type
         self.input_size = in_size
         self.last_layer_size = self.input_size
 
+    # API functions
+
+    # API function for adding a hidden layer with specified size
     def add_layer(self, layer_size, W=None, b=None):
         prev_layer_size = self.last_layer_size
 
         # 1.3.12
-        # generating the parameters randomly as requested
+        # generating the parameters randomly if not given
         if W is None:
             W = np.random.randn(prev_layer_size, layer_size) / np.sqrt(layer_size)
         if b is None:
@@ -64,6 +41,21 @@ class My_FC_NN_Model(object):
         self.layers[f'l{self.layer_index}']['b'] = b
         self.last_layer_size = layer_size
 
+    # API function for setting loss type:
+    # 'mse' is the default
+    def set_loss_type(self, loss_type):
+        self.loss_type = loss_type
+
+    # API function for setting activation type:
+    # 'tanh' is the default
+    def set_activation_type(self, activation_type):
+        self.activation_type = activation_type
+
+    # API function for model prediction (wrapping above fwd)
+    def predict(self, input_vec):
+        return self.fwd(input_vec)
+
+    # API function for printing the model layers' params
     def print(self):
         for i in np.arange(1,self.layer_index+1):
             print(f"layer {i}:")
@@ -72,6 +64,10 @@ class My_FC_NN_Model(object):
             b = self.layers[f'l{i}']['b']
             print(f"b:\n{b}")
 
+    # Internal functions
+
+    # 1.3.5
+    # Forwarding an input sample through the model layers
     def fwd(self, input_vec):
         layers_outputs = {}
         next_layer_input = input_vec
@@ -80,14 +76,46 @@ class My_FC_NN_Model(object):
             b = self.layers[f'l{i}']['b']
             y = np.dot(W.T, next_layer_input) + b
             layers_outputs[f'l{i}_lin'] = y
-            next_layer_input = phi(y)
+            next_layer_input = self.phi(y)
             layers_outputs[f'l{i}'] = next_layer_input
         return layers_outputs, y
+
+    # 1.3.6
+    def phi(self, x):
+        act_out = None
+        if self.activation_type == 'tanh':
+            act_out = np.tanh(x)
+        else:
+            assert 0, f'the activation type {self.activation_type} is not supported'
+        return act_out
+
+    def g_phi(self, x):
+        g_act_out = None
+        if self.activation_type == 'tanh':
+            g_act_out = 1 - (np.tanh(x) ** 2)
+        else:
+            assert 0, f'the activation type {self.activation_type} is not supported'
+        return g_act_out
+
+    # 1.3.7
+    def loss(self, y, F):
+        if self.loss_type == 'mse':
+            L = (F - y)**2
+        else:
+            assert 0, f'the loss type {self.loss_type} is not supported'
+        return L
+
+    def loss_grad(self, y, F):
+        if self.loss_type == 'mse':
+            L_der = 2 * (F - y)
+        else:
+            assert 0, f'the loss type {self.loss_type} is not supported'
+        return L_der
 
     # 1.3.8
     def fwd_and_backprop(self, input_vec, label):
         outputs, y_hat = self.fwd(input_vec)
-        grad_z = self.calc_loss_grad(label, y_hat)
+        grad_z = self.loss_grad(label, y_hat)
         grads = {}
         for li in np.arange(start=self.layer_index, stop=0, step=-1):
             if li > 1:
@@ -100,7 +128,7 @@ class My_FC_NN_Model(object):
             # if we're not in the last layer then we have an activation function
             # and thus it's gradient needs to be considered
             if li < self.layer_index:
-                grad_act = g_phi(y)
+                grad_act = self.g_phi(y)
                 phi_grad_diag = np.diagflat(grad_act)
             else:
                 phi_grad_diag = np.expand_dims(1, axis=(0, 1))
@@ -127,23 +155,6 @@ class My_FC_NN_Model(object):
         grads_delta = grads_sum / n
         return grads_delta
 
-    def set_loss_type(self, loss_type):
-        self.loss_type = loss_type
-
-    def calc_loss(self, y, y_hat):
-        if self.loss_type == 'mse':
-            L = loss(y_hat,y)
-        else:
-            assert(0, f'the loss type {self.loss_type} is not supported')
-        return L
-
-    def calc_loss_grad(self, y, y_hat):
-        if self.loss_type == 'mse':
-            L_der = g_loss(y_hat,y)
-        else:
-            assert(0, f'the loss type {self.loss_type} is not supported')
-        return L_der
-
 
 # 1.3.10,11 - generating data samples for training and testing
 def gen_samples(func_ptr, n):
@@ -152,6 +163,7 @@ def gen_samples(func_ptr, n):
     for sample in samples:
         labels.append(func_ptr(sample[0], sample[1]))
     return samples, np.array(labels)
+
 
 # 1.3.13
 def plot_func(func_ptr, title="", model=None, plot_samples=False, samples=None, labels=None):
@@ -194,6 +206,15 @@ def plot_func(func_ptr, title="", model=None, plot_samples=False, samples=None, 
 
     plt.show()
 
+
+# helper function to build the model layers
+def build_model(in_size, hidden_layers_sizes):
+    nn_model = My_FC_NN_Model(in_size=in_size)
+    for li in np.arange(1,len(hidden_layers_sizes)+1):
+        nn_model.add_layer(layer_size=hidden_layers_sizes[li-1])
+    return nn_model
+
+
 if __name__ == '__main__':
     run_rosenbrock_BFGS = False
     if run_rosenbrock_BFGS:
@@ -207,6 +228,42 @@ if __name__ == '__main__':
         title += r'$log{(f(x_k)-f(x^*))}$ vs number of iterations'
         rosenbrock_plot(trajectory_points, title)
 
+    in_size = 2
+    hidden_layers_sizes = (4,3,1)
+    nn_model = build_model(in_size=in_size, hidden_layers_sizes=hidden_layers_sizes)
+    nn_model.print()
+
+    train_samples, train_labels = gen_samples(f, 500)
+    #plot_func(f, plot_samples=True, samples=train_samples, labels=train_labels)
+
+    test_samples, test_labels = gen_samples(f, 200)
+    plot_func(f, plot_samples=True, samples=test_samples, labels=test_labels)
+
+    run_func_approximation = False
+    if run_func_approximation:
+        epsilons = [1e-1, 1e-2, 1e-3, 1e-4]
+
+        for epsilon in epsilons:
+            print(f'Optimizing model with epsilon = {epsilon}')
+            print(f'Start training')
+            #w_opt, values_list, points_list = BFGS(loss, batch_forward_backward, w, eps, x_train, y_train)
+            #plot(forward, x_train, y_train, False, f'F(x,W*) using epsilon={eps}', w_opt)
+            print(f'Ended training')
+            #print(f'Trained examples Loss = {values_list[-1]}')
+            print(f'Start testing')
+            predictions = []
+            for x in test_samples:
+                _, prediction = nn_model.predict(x)
+                predictions.append(prediction)
+            plot_func(f,
+                      title=f'Predicted test samples with epsilon {epsilon}',
+                      plot_samples=True,
+                      samples=test_samples,
+                      labels=predictions)
+            print('Ended testing')
+
+
+    '''
     w_vec = np.arange(0,31)
     print(w_vec)
     in_size = 2
@@ -218,15 +275,15 @@ if __name__ == '__main__':
     prev_layer_size = in_size
     for i in np.arange(1,len(layers_sizes_dict)+1):
         layer_size = layers_sizes_dict[f'l{layer_index}']
-        print(f"layer size: {layer_size}")
+        #print(f"layer size: {layer_size}")
         start_w = start_offset
-        print(f"start_w: {start_w}")
+        #print(f"start_w: {start_w}")
         end_w = start_w + prev_layer_size * layer_size
-        print(f"end_w: {end_w}")
+        #print(f"end_w: {end_w}")
         start_b = end_w
-        print(f"start_b: {start_b}")
+        #print(f"start_b: {start_b}")
         end_b = start_b + layer_size
-        print(f"end_b: {end_b}")
+        #print(f"end_b: {end_b}")
         nn_model.add_layer(
             layer_size=layer_size,
             W=w_vec[start_w:end_w].reshape((prev_layer_size, layer_size)),
@@ -235,9 +292,6 @@ if __name__ == '__main__':
         start_offset = end_b
         prev_layer_size = layer_size
         layer_index += 1
-
-    nn_model.print()
-
     fwd_outputs, y_estimate = nn_model.fwd([[1], [2]])
     print(f"fwd_outputs: {fwd_outputs}")
     print(f"y_estimate: {y_estimate}")
@@ -245,11 +299,4 @@ if __name__ == '__main__':
     print(f"fwd_bwd_outputs: {fwd_bwd_outputs}")
     print(f"gradients_vec {gradients_vec}")
     print(f"y_estimate {y_estimate}")
-
-
-    train_samples, train_labels = gen_samples(f, 500)
-    #plot_func(f, plot_samples=True, samples=train_samples, labels=train_labels)
-
-    test_samples, test_labels = gen_samples(f, 200)
-    plot_func(f, plot_samples=True, samples=test_samples, labels=test_labels)
-
+    '''
