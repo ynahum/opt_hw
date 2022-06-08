91076ea099cb9718914c49568d6e4e9219d0a451
Yair Nahum <yair.y.nahum@intel.com>
2022-06-09 00:12:33 +0300

    hw4 add penalty func and its derivatives

---
+++ hw4/penalty.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from newton_method_utils import *
+
+
+def penalty_func(t):
+    if t >= -0.5:
+        f = 0.5*(t**2) + t
+    else:
+        f = -0.25*np.log(-2*t) - 0.375
+    return f
+
+
+def penalty_first_derivative(t):
+    if t >= -0.5:
+        f = t + 1
+    else:
+        f = -0.25/t
+    return f
+
+
+def penalty_second_derivative(t):
+    if t >= -0.5:
+        f = 1
+    else:
+        f = 0.25/(t**2)
+    return f
+
+
